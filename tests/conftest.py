3f6cec8fefa4102e7e87eac36093e1dd84a60558
ILgiza <ilgizagalievna66580@gmail.com>
2024-10-22 15:20:41 +0500

    change of tested site

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,23 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: en, ru")
+
 
 @pytest.fixture(scope="function")
-def browser():
+def browser(request):
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
+
+
+
